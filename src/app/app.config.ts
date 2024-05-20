66d865f023aad60eb4a20e5a557ef175b0b5428a
Andrés Quintero Arias <andres.quintero@xpertgroup.co>
2024-05-20 15:16:36 -0500

    📦 Configuration:  Added PWA support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
     provideRouter(routes, withViewTransitions(), withComponentInputBinding()),
     provideHttpClient(withInterceptors([])),
+    provideServiceWorker('ngsw-worker.js', {
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
 };
